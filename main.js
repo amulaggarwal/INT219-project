8ed023cf17abf16672132c59fb686e43ed73ce2c
amulaggarwal <aggarwalamul6101@gmail.com>
2021-11-28 11:55:05 +0530

    implemented show/hide menu for mobile ui

--- main.js
+++ main.js
@@ -5,3 +5,25 @@ const swiper = new Swiper('.swiper', {
       draggable: true,
     },
   });
+
+
+  const menuBtn = document.querySelector('#menu-btn');
+  const closeBtn = document.querySelector('#close-btn');
+  const menu = document.querySelector('nav .container ul');
+
+  
+
+//SHOW MENU
+  menuBtn.addEventListener('click',()=>{
+      menu.style.display = 'block';
+      menuBtn.style.display = 'none';
+      closeBtn.style.display = 'inline-block';
+  })
+
+
+//HIDE MENU
+  closeBtn.addEventListener('click',()=>{
+    menu.style.display = 'none';
+    menuBtn.style.display = 'inline-block';
+    closeBtn.style.display = 'none';
+  })
