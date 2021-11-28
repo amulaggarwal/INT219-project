ebccb21ddfe74414ec4588a5f6f904015771a0bb
amulaggarwal <aggarwalamul6101@gmail.com>
2021-11-28 16:58:26 +0530

    implemented javascript functionality

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ const swiper = new Swiper('.swiper', {
   const closeBtn = document.querySelector('#close-btn');
   const menu = document.querySelector('nav .container ul');
 
-  
+
 
 //SHOW MENU
   menuBtn.addEventListener('click',()=>{
@@ -27,3 +27,51 @@ const swiper = new Swiper('.swiper', {
     menuBtn.style.display = 'inline-block';
     closeBtn.style.display = 'none';
   })
+
+
+ const navItems = menu.querySelectorAll('li'); 
+ const changeActiveItem = ()=>{
+    navItems.forEach(item =>{
+        const link = item.querySelector('a');
+        link.classList.remove('active')
+    }) 
+ }
+
+ navItems.forEach(item=>{
+     const link = item.querySelector('a');
+     link.addEventListener('click',()=>{
+         changeActiveItem();
+         link.classList.add('active');
+     })
+ })
+
+
+ //read more about
+ const readMoreBtn = document.querySelector('.read-more');
+ const readMoreContent = document.querySelector('.read-more-content');
+
+ readMoreBtn.addEventListener('click',()=>{
+     readMoreContent.classList.toggle('show-content')
+     if(readMoreContent.classList.contains('show-content')){
+         readMoreBtn.textContent = "Show less";
+     }else{
+        readMoreBtn.textContent = "Show more";
+     }
+ })
+
+
+ // show/hide skills items
+
+
+ const skillItems = document.querySelectorAll('section.skills .skill');
+
+ skillItems.forEach(skill=>{
+     skill.querySelector('.head').addEventListener('click',()=>{
+         skill.querySelector('.items').classList.toggle('show-items');
+     })
+ })
+
+ //add box shadow on scroll
+ window.addEventListener('scroll',()=>{
+     document.querySelector('nav').classList.toggle('show-box-shadow',window.scrollY>0)
+ })
